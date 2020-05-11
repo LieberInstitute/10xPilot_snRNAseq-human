4967a101c77664f2f44289e72ee66bbf3b6cff12
Matt N Tran <nnguye44@jhmi.edu>
2020-05-10 23:41:07 -0400

    Correlation of ts across all regions from specificity modeling of cluster-specific genes with single-nucleus-level test ('cluster-vs-all-others')

---
+++ 10x-pilot_panBrain_cross-region-analyses_correlations_MNTMay2020.R
@@ -0,0 +1,230 @@
+### MNT 10x snRNA-seq workflow: step 04 - downstream comparisons
+###   **Pan-brain analyses**
+###     - n=12 samples from 5 regions, up to three donors
+###   * Cross-region analysis/correlation and comp. to other datasets
+#####################################################################
+
+library(SingleCellExperiment)
+library(EnsDb.Hsapiens.v86)
+library(org.Hs.eg.db)
+library(scater)
+library(scran)
+library(batchelor)
+library(DropletUtils)
+library(jaffelab)
+library(limma)
+library(lattice)
+library(RColorBrewer)
+library(pheatmap)
+
+### Palette taken from `scater`
+tableau10medium = c("#729ECE", "#FF9E4A", "#67BF5C", "#ED665D",
+                    "#AD8BC9", "#A8786E", "#ED97CA", "#A2A2A2",
+                    "#CDCC5D", "#6DCCDA")
+tableau20 = c("#1F77B4", "#AEC7E8", "#FF7F0E", "#FFBB78", "#2CA02C",
+              "#98DF8A", "#D62728", "#FF9896", "#9467BD", "#C5B0D5",
+              "#8C564B", "#C49C94", "#E377C2", "#F7B6D2", "#7F7F7F",
+              "#C7C7C7", "#BCBD22", "#DBDB8D", "#17BECF", "#9EDAE5")
+
+# ===
+
+load("rdas/markers-stats_DLPFC_n2_findMarkers-SN-LEVEL_MNTApr2020.rda", verbose=T)
+    # markers.dlpfc.t.1vAll, markers.dlpfc.t.design
+
+load("rdas/markers-stats_HPC-n3_findMarkers-SN-LEVEL_MNTMay2020.rda", verbose=T)
+    # markers.hpc.t.1vAll, markers.hpc.t.design, markers.hpc.wilcox.block
+
+load("rdas/markers-stats_NAc-n5_findMarkers-SN-LEVEL_MNTApr2020.rda", verbose=T)
+    # markers.nac.t.design, markers.nac.t.1vAll
+
+load("rdas/markers-stats_Amyg-n2_findMarkers-SN-LEVEL_MNTMay2020.rda", verbose=T)
+    # markers.amy.t.1vAll, markers.amy.t.design, markers.amy.wilcox.block
+
+load("rdas/markers-stats_sACC-n2_findMarkers-SN-LEVEL_MNTMay2020.rda", verbose=T)
+    # markers.sacc.t.design, markers.sacc.wilcox.block, markers.sacc.binom.block, markers.sacc.t.1vAll
+
+    # Remove all pairwise stats
+    rm(list=setdiff(ls(), ls(pattern=".1vAll")))
+
+
+# Re-order rows in each list entry for each set of stats
+expressedGenes.list <- list(amy=rownames(markers.amy.t.1vAll[["Astro"]]),
+                            dlpfc=rownames(markers.dlpfc.t.1vAll[["Astro"]]),
+                            hpc=rownames(markers.hpc.t.1vAll[["Astro"]]),
+                            nac=rownames(markers.nac.t.1vAll[["Astro"]]),
+                            sacc=rownames(markers.sacc.t.1vAll[["Astro"]])
+                            )
+
+expressedGenes <- unique(unlist(expressedGenes.list))
+
+expressedGenes <- expressedGenes[expressedGenes %in% expressedGenes.list[["amy"]] &
+                                   expressedGenes %in% expressedGenes.list[["dlpfc"]] &
+                                   expressedGenes %in% expressedGenes.list[["nac"]] &
+                                   expressedGenes %in% expressedGenes.list[["hpc"]] &
+                                   expressedGenes %in% expressedGenes.list[["sacc"]]
+                                 ]
+length(expressedGenes)  # 26888
+
+# Store each set of stats into a list
+FMstats.list <- list(amy=markers.amy.t.1vAll,
+                     dlpfc=markers.dlpfc.t.1vAll,
+                     hpc=markers.hpc.t.1vAll,
+                     nac=markers.nac.t.1vAll,
+                     sacc=markers.sacc.t.1vAll)
+
+    # How many subclusters in each region?
+    sapply(FMstats.list, length)
+        # amy dlpfc   hpc   nac  sacc
+        #  12    17    15    14    10
+    
+    sapply(FMstats.list, function(x){nrow(x[["Astro"]])})
+        #  amy dlpfc   hpc   nac  sacc
+        #28464 28111 28757 29236 28774
+
+# Subset and re-order for those intersecting genes across all regions
+for(x in names(FMstats.list)){
+  for(s in names(FMstats.list[[x]])){
+    FMstats.list[[x]][[s]] <- FMstats.list[[x]][[s]][expressedGenes, ]
+  }
+}
+
+
+sapply(FMstats.list, function(x){nrow(x[["Astro"]])})
+    # good
+sapply(FMstats.list, function(x){head(x[["Astro"]], n=3)})
+
+
+
+
+
+
+
+### Get n Nuclei numbers for each region so can compute t-statistics ===
+  # This can be done with Cohen's D (the 'std.lfc'), as d = t/sqrt(N)
+
+load("rdas/all-FACS-homogenates_n12_PAN-BRAIN-Analyses_MNTFeb2020.rda", verbose=T)
+    # sce.all.n12, chosen.hvgs.all.n12, pc.choice.n12, ref.sampleInfo, clusterRefTab.all.n12
+    rm(chosen.hvgs.all.n12, pc.choice.n12, clusterRefTab.all.n12, ref.sampleInfo)
+
+sce.all.n12
+    # class: SingleCellExperiment
+    # dim: 33538 34070    
+table(sce.all.n12$cellType.RS)
+
+# Get rid of those which were 'Ambig.lowNtrxts' bc removed before cluster-specificity tests run
+sce.all.n12 <- sce.all.n12[ ,!sce.all.n12$cellType.RS=="Ambig.lowNtrxts"]
+sampleNumNuclei <- table(sce.all.n12$region)
+# Remove NAc because these clusters were defined from all-n5-sample region analysis
+sampleNumNuclei <- sampleNumNuclei[c("amy","dlpfc","hpc","sacc")]
+
+# Get N nuclei from all-n=5-NAc dataset
+load("rdas/regionSpecific_NAc-ALL-n5_cleaned-combined_SCE_MNTMar2020.rda", verbose=T)
+    # sce.nac.all, chosen.hvgs.nac.all, pc.choice.nac.all, clusterRefTab.nac.all, ref.sampleInfo
+    rm(chosen.hvgs.nac.all, pc.choice.nac.all, clusterRefTab.nac.all)
+
+sampleNumNuclei.nac <- table(sce.nac.all$region) - table(sce.nac.all$cellType.final=="ambig.lowNtrxts")["TRUE"]
+    #   nac
+    # 13148
+
+sampleNumNuclei <- c(sampleNumNuclei, sampleNumNuclei.nac)
+    #  amy dlpfc   hpc  sacc   nac
+    # 6582  5231 10343  7004 13148
+
+
+## Calculate and add t-statistic (= std.logFC * sqrt(N))
+for(x in names(FMstats.list)){
+  for(s in names(FMstats.list[[x]])){
+    FMstats.list[[x]][[s]]$t.stat <- FMstats.list[[x]][[s]]$std.logFC * sqrt(sampleNumNuclei[x])
+  }
+}
+
+
+## Let's save these
+readme.mnt <- "These stats are from region-specific specificity modeling (cluster-vs-all-others) at the single-nucleus level with 'scran::findMarkers()'. The t-statistic is computed by sqrt(N.nuclei) * std.logFC."
+save(FMstats.list, sampleNumNuclei, readme.mnt, ref.sampleInfo,
+     file="rdas/markers-stats_SN-LEVEL_1vAll_all-regions-combined_May2020.rda")
+
+
+# (If needed)
+#load("rdas/markers-stats_SN-LEVEL_1vAll_all-regions-combined_May2020.rda", verbose=T)
+#readme.mnt
+
+## Create matrix of t's with region:subcluster identifiers
+ts.list <- lapply(FMstats.list, function(x){
+  sapply(x, function(y){y$t.stat})
+  }
+)
+# Add back in row names and region suffix
+for(i in names(ts.list)){
+  rownames(ts.list[[i]]) <- rownames(FMstats.list[[1]][[1]])
+  colnames(ts.list[[i]]) <- paste0(colnames(ts.list[[i]]), "_", i)
+}
+# Cbind
+ts.fullMat <- do.call(cbind, ts.list)
+
+
+## Correlation; first shorten names
+colnames(ts.fullMat) <- gsub("Excit", "Ex", colnames(ts.fullMat))
+colnames(ts.fullMat) <- gsub("Inhib", "In", colnames(ts.fullMat))
+cor_t_panBrain <- cor(ts.fullMat)
+
+
+
+
+
+
+
+
+
+
+### Heatmap (pasted script - for using levelplot, but will want pheatmap bc will cluster cols/rows)
+theSeq.all = seq(-1, 1, by = 0.025)
+my.col.all <- colorRampPalette(brewer.pal(7, "PRGn"))(length(theSeq.all)-1)
+
+
+pdf("pdfs/exploration/pan-Brain_correlation_region-specific-subcluster-ts_May2020.pdf")
+pheatmap(cor_t_panBrain,
+         color=my.col.all,
+         breaks=theSeq.all,
+         fontsize_row=6.5, fontsize_col=6.5,
+         main="Correlation of cluster-specific t's from all regions \n (all shared expressed genes)")
+dev.off()
+
+
+
+
+
+        # ## MNT added 14Apr2020: Reorder to diagonal & threshold at 0.4 for all-gene correlation === === ===
+        # # Start from descending - easier to manually order
+        # #cor_t_all_toPlot <- cor_t_all_toPlot[ ,rev(1:ncol(cor_t_all_toPlot))]
+        # # This is useful:
+        # apply(cor_t_all_toPlot, 2, which.max)
+        # # If want to re-order human labels (but prefer re-ordering mm labels)
+        # #cor_t_all_toPlot <- cor_t_all_toPlot[ ,rev(c(14,5,3,4, 7,10,12,6, 9,8,2,1, 11,13))]
+        # cor_t_all_toPlot <- cor_t_all_toPlot[c(14,11:13,4,3, 2,8,7,9, 6,15,5,16,1,10), ]
+        # # Threshold at 0.4
+        # range(cor_t_all_toPlot)
+        # cor_t_all_toPlot <- ifelse(cor_t_all_toPlot >= 0.4, 0.4, cor_t_all_toPlot)
+        # 
+        # 
+        # #pdf("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/pdfs/exploration/overlap-mouse-MeA_with_LIBD-10x-Amyg_top100-or-all_Apr2020.pdf")
+        # # Most human-specific
+        # print(
+        #   levelplot(
+        #     cor_t_panBrain,
+        #     aspect = "fill",
+        #     at = theSeq.all,
+        #     col.regions = my.col.all,
+        #     ylab = "",
+        #     xlab = "",
+        #     scales = list(x = list(rot = 90, cex = 1), y = list(cex = 1)),
+        #     main="Correlation of cluster-specific t's from all regions \n (all shared expressed genes)"
+        #   )
+        # )
+        # #[...]
+        # #dev.off()
+
+
+
+
+
