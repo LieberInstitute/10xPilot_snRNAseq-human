25a312d2776121a0435281c3f85d415ee3fe41aa
Arta <arta.seyedian@gmail.com>
2020-08-26 13:55:47 -0400

    added an elif chain to build_bims wrapper
    I got an error when I tried to qsub build_bims_NAc_genes.sh without command line arguments... prevented the R script from working (caused some weird character encoding error). So I made an elif chain to make it fully accept the R script's default arguments in the case that somebody submits without adding arguments.

--- twas/build_bims_NAc_genes.sh
+++ twas/build_bims_NAc_genes.sh
@@ -27,7 +27,22 @@ module load conda_R/4.0
 module list
 
 ## Compute weights for the given region/feature pair
-Rscript build_bims.R -c 12 -t $1 -d $2
+## elif chain to preclude the possibility of any commandline-argument-passing error
+## if arg 1 is not null and arg 2 is not null... etc.
+if [ ! -z "$1" ] && [ ! -z "$2"];
+then
+  Rscript build_bims.R -c 12 -t $1 -d $2;
+elif [ ! -z "$1" ] && [ -z "$2"];
+then
+  Rscript build_bims.R -c 12 -t $1;
+elif [ -z "$1" ] && [ ! -z "$2"];
+then
+  Rscript build_bims.R -c 12 -d $2;
+else
+  Rscript build_bims.R -c 12;
+fi
+
+
 
 echo "**** Job ends ****"
 date
