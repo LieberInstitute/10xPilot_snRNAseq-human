3e870544d48da60d48d06bd693d8ed07851a40be
lcolladotor <lcollado@jhsph.edu>
2020-09-04 15:56:04 -0400

    Fix the NAc sample selection, although 1 of the brains that has mds is missing from this selection
    ```
    > match(brnumerical(rownames(mds)), brnumerical(newbfile_fam$famid))
      [1]   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16  17  18
     [19]  19  20  21  23  24  26  27  28  29  30  31  32  33  34  35  36  37  38
     [37]  39  40  41  42  43  44  45  46  47  48  49  50  51  52  53  55  56  57
     [55]  59  60  61  62  63  64  65  66  67  68  69  70  71  72  74  75  76  77
     [73]  78  79  80  81  82  83  84  85  86  88  89  90  91  93  94  95  96  97
     [91]  NA  98  99 100 101 103 104 105 106 107 108 109 110 111 112 113 114 115
    [109] 116 117 118 119 120 121 123 124 125 126 127 128 129 131 132 133 134 135
    [127] 136 137 138 139 140 141 142 143 144 145 146 147 149 150 151 152 153 161
    [145] 154 155 156 157 158 159 160 162 148 164 165 166
    ```

--- twas/filter_data/filter_snps.R
+++ twas/filter_data/filter_snps.R
@@ -34,8 +34,13 @@ setkey(libd_fam, 'brnumerical')
 message(paste(Sys.time(), 'processing', "NAc_Nicotine"))
 samp_file <- paste0('samples_to_extract_', "NAc_Nicotine", '.txt')
 
+## Which NAc samples have genotype data?
+filter_m <- match(brnumerical(rse_gene$BrNum), libd_fam$brnumerical)
+message(Sys.time(), " NAc BrNums that have genotype data")
+table(!is.na(filter_m))
+
 fwrite(
-    libd_fam[brnumerical(rse_gene$BrNum), 1:2], ## can be more involved
+    libd_fam[filter_m[!is.na(filter_m)], 1:2], ## can be more involved
     file = samp_file,
     sep = '\t', col.names = FALSE
 )
@@ -49,7 +54,6 @@ newbfile <- here::here("twas", "filter_data", "duplicate_snps_bim", paste0(
 
 ## Extract
 message(paste(Sys.time(), 'running bfile extract for', newbfile))
-
 system(paste("plink --bfile", libd_bfile,
 	"--keep", samp_file, "--make-bed --out",
 	newbfile, " --memory 100000 --biallelic-only"))
@@ -58,8 +62,8 @@ system(paste("plink --bfile", libd_bfile,
 newbfile_fam <- fread(paste0(newbfile, ".fam"),
     col.names = c("famid", "w_famid", "w_famid_fa", "w_famid_mo", "sex_code", "phenotype")
 )
-m <- match(brnumerical(newbfile_fam$famid), brnumerical(colData(rse_gene)$BrNum))
-stopifnot(all(!is.na(m)))
+check_m <- match(brnumerical(newbfile_fam$famid), brnumerical(colData(rse_gene)$BrNum))
+stopifnot(all(!is.na(check_m)))
 
 
 ## Re-run but now make the SNV names unique
@@ -71,7 +75,6 @@ newbfile_unique <- here::here("twas", "filter_data", "unique_snps_bim", paste0(
 
 ## Extract again (could also copy and rename, but it's fast this way)
 message(paste(Sys.time(), 'running bfile extract for', newbfile_unique))
-
 system(paste("plink --bfile", libd_bfile,
              "--keep", samp_file, "--make-bed --out",
              newbfile_unique, " --memory 100000 --biallelic-only"))
@@ -102,46 +105,79 @@ Sys.time()
 proc.time()
 options(width = 120)
 session_info()
-# ??? Session info ?????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-#  setting  value
-#  version  R version 4.0.2 Patched (2020-06-24 r78746)
-#  os       CentOS Linux 7 (Core)
-#  system   x86_64, linux-gnu
-#  ui       X11
-#  language (EN)
-#  collate  en_US.UTF-8
-#  ctype    en_US.UTF-8
-#  tz       US/Eastern
-#  date     2020-08-27
-#
-# ??? Packages ?????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-#  package              * version  date       lib source
-#  assertthat             0.2.1    2019-03-21 [2] CRAN (R 4.0.0)
-#  Biobase              * 2.48.0   2020-04-27 [2] Bioconductor
-#  BiocGenerics         * 0.34.0   2020-04-27 [2] Bioconductor
-#  bitops                 1.0-6    2013-08-17 [2] CRAN (R 4.0.0)
-#  cli                    2.0.2    2020-02-28 [2] CRAN (R 4.0.0)
-#  crayon                 1.3.4    2017-09-16 [2] CRAN (R 4.0.0)
-#  data.table           * 1.12.8   2019-12-09 [2] CRAN (R 4.0.0)
-#  DelayedArray         * 0.14.0   2020-04-27 [2] Bioconductor
-#  fansi                  0.4.1    2020-01-08 [2] CRAN (R 4.0.0)
-#  GenomeInfoDb         * 1.24.0   2020-04-27 [2] Bioconductor
-#  GenomeInfoDbData       1.2.3    2020-05-18 [2] Bioconductor
-#  GenomicRanges        * 1.40.0   2020-04-27 [2] Bioconductor
-#  glue                   1.4.1    2020-05-13 [2] CRAN (R 4.0.0)
-#  IRanges              * 2.22.1   2020-04-28 [2] Bioconductor
-#  lattice                0.20-41  2020-04-02 [3] CRAN (R 4.0.2)
-#  Matrix                 1.2-18   2019-11-27 [3] CRAN (R 4.0.2)
-#  matrixStats          * 0.56.0   2020-03-13 [2] CRAN (R 4.0.0)
-#  RCurl                  1.98-1.2 2020-04-18 [2] CRAN (R 4.0.0)
-#  S4Vectors            * 0.26.1   2020-05-16 [2] Bioconductor
-#  sessioninfo          * 1.1.1    2018-11-05 [2] CRAN (R 4.0.0)
-#  SummarizedExperiment * 1.18.1   2020-04-30 [2] Bioconductor
-#  withr                  2.2.0    2020-04-20 [2] CRAN (R 4.0.0)
-#  XVector                0.28.0   2020-04-27 [2] Bioconductor
-#  zlibbioc               1.34.0   2020-04-27 [2] Bioconductor
-#
-# [1] /users/aseyedia/R/4.0
+# ─ Session info ───────────────────────────────────────────────────────────────────────────────────────────────────────
+# setting  value                                      
+# version  R version 4.0.2 Patched (2020-06-24 r78746)
+# os       CentOS Linux 7 (Core)                      
+# system   x86_64, linux-gnu                          
+# ui       X11                                        
+# language (EN)                                       
+# collate  en_US.UTF-8                                
+# ctype    en_US.UTF-8                                
+# tz       US/Eastern                                 
+# date     2020-09-04                                 
+# 
+# ─ Packages ───────────────────────────────────────────────────────────────────────────────────────────────────────────
+# package              * version  date       lib source                            
+# assertthat             0.2.1    2019-03-21 [2] CRAN (R 4.0.0)                    
+# backports              1.1.6    2020-04-05 [1] CRAN (R 4.0.0)                    
+# Biobase              * 2.48.0   2020-04-27 [1] Bioconductor                      
+# BiocGenerics         * 0.34.0   2020-04-27 [1] Bioconductor                      
+# bitops                 1.0-6    2013-08-17 [2] CRAN (R 4.0.0)                    
+# cli                    2.0.2    2020-02-28 [1] CRAN (R 4.0.0)                    
+# colorout             * 1.2-2    2020-05-08 [1] Github (jalvesaq/colorout@726d681)
+# colorspace             1.4-1    2019-03-18 [2] CRAN (R 4.0.0)                    
+# crayon                 1.3.4    2017-09-16 [1] CRAN (R 4.0.0)                    
+# data.table           * 1.12.8   2019-12-09 [1] CRAN (R 4.0.0)                    
+# DelayedArray         * 0.14.0   2020-04-27 [1] Bioconductor                      
+# digest                 0.6.25   2020-02-23 [1] CRAN (R 4.0.0)                    
+# dplyr                  0.8.5    2020-03-07 [1] CRAN (R 4.0.0)                    
+# ellipsis               0.3.0    2019-09-20 [1] CRAN (R 4.0.0)                    
+# fansi                  0.4.1    2020-01-08 [1] CRAN (R 4.0.0)                    
+# GenomeInfoDb         * 1.24.0   2020-04-27 [1] Bioconductor                      
+# GenomeInfoDbData       1.2.3    2020-05-18 [2] Bioconductor                      
+# GenomicRanges        * 1.40.0   2020-04-27 [1] Bioconductor                      
+# ggplot2                3.3.0    2020-03-05 [1] CRAN (R 4.0.0)                    
+# glue                   1.4.0    2020-04-03 [1] CRAN (R 4.0.0)                    
+# gtable                 0.3.0    2019-03-25 [2] CRAN (R 4.0.0)                    
+# here                 * 0.1      2017-05-28 [1] CRAN (R 4.0.0)                    
+# htmltools              0.4.0    2019-10-04 [1] CRAN (R 4.0.0)                    
+# htmlwidgets            1.5.1    2019-10-08 [1] CRAN (R 4.0.0)                    
+# httpuv                 1.5.2    2019-09-11 [1] CRAN (R 4.0.0)                    
+# IRanges              * 2.22.1   2020-04-28 [1] Bioconductor                      
+# jsonlite               1.6.1    2020-02-02 [2] CRAN (R 4.0.0)                    
+# later                  1.0.0    2019-10-04 [1] CRAN (R 4.0.0)                    
+# lattice                0.20-41  2020-04-02 [3] CRAN (R 4.0.2)                    
+# lifecycle              0.2.0    2020-03-06 [1] CRAN (R 4.0.0)                    
+# magrittr               1.5      2014-11-22 [1] CRAN (R 4.0.0)                    
+# Matrix                 1.2-18   2019-11-27 [3] CRAN (R 4.0.2)                    
+# matrixStats          * 0.56.0   2020-03-13 [1] CRAN (R 4.0.0)                    
+# munsell                0.5.0    2018-06-12 [2] CRAN (R 4.0.0)                    
+# pillar                 1.4.4    2020-05-05 [1] CRAN (R 4.0.0)                    
+# pkgconfig              2.0.3    2019-09-22 [1] CRAN (R 4.0.0)                    
+# png                    0.1-7    2013-12-03 [2] CRAN (R 4.0.0)                    
+# promises               1.1.0    2019-10-04 [1] CRAN (R 4.0.0)                    
+# purrr                  0.3.4    2020-04-17 [1] CRAN (R 4.0.0)                    
+# R6                     2.4.1    2019-11-12 [2] CRAN (R 4.0.0)                    
+# Rcpp                   1.0.4.6  2020-04-09 [1] CRAN (R 4.0.0)                    
+# RCurl                  1.98-1.2 2020-04-18 [2] CRAN (R 4.0.0)                    
+# rlang                  0.4.6    2020-05-02 [1] CRAN (R 4.0.0)                    
+# rmote                * 0.3.4    2020-05-08 [1] Github (cloudyr/rmote@fbce611)    
+# rprojroot              1.3-2    2018-01-03 [2] CRAN (R 4.0.0)                    
+# S4Vectors            * 0.26.0   2020-04-27 [1] Bioconductor                      
+# scales                 1.1.1    2020-05-11 [2] CRAN (R 4.0.0)                    
+# servr                  0.16     2020-03-02 [1] CRAN (R 4.0.0)                    
+# sessioninfo          * 1.1.1    2018-11-05 [1] CRAN (R 4.0.0)                    
+# SummarizedExperiment * 1.18.1   2020-04-30 [1] Bioconductor                      
+# tibble                 3.0.1    2020-04-20 [1] CRAN (R 4.0.0)                    
+# tidyselect             1.1.0    2020-05-11 [2] CRAN (R 4.0.0)                    
+# vctrs                  0.2.4    2020-03-10 [1] CRAN (R 4.0.0)                    
+# withr                  2.2.0    2020-04-20 [1] CRAN (R 4.0.0)                    
+# xfun                   0.13     2020-04-13 [1] CRAN (R 4.0.0)                    
+# XVector                0.28.0   2020-04-27 [1] Bioconductor                      
+# zlibbioc               1.34.0   2020-04-27 [1] Bioconductor                      
+# 
+# [1] /users/lcollado/R/4.0
 # [2] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.0/R/4.0/lib64/R/site-library
 # [3] /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.0/R/4.0/lib64/R/library
 system('plink --version')
