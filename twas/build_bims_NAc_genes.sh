4cbd3f09b16dfa0da37229e362da8b3485032d2d
lcolladotor <lcollado@jhsph.edu>
2020-09-04 18:19:08 -0400

    Ran a successful qsub test. Max memory was ~75Gb though it was around 2GB initially. Likely has to do with plink mem (5 GB per file right now).
    Info from SGE email:
    
    ```
    Job 2410287 (build_bims_NAc_genes) Complete
     User             = lcollado
     Queue            = bluejay.q@compute-097.cm.cluster
     Host             = compute-097.cm.cluster
     Start Time       = 09/04/2020 18:14:55
     End Time         = 09/04/2020 18:17:51
     User Time        = 00:04:31
     System Time      = 00:01:49
     Wallclock Time   = 00:02:56
     CPU              = 00:06:21
     Max vmem         = 74.931G
     Exit Status      = 0
    ```

--- twas/build_bims_NAc_genes.sh
+++ twas/build_bims_NAc_genes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -cwd
-#$ -l mem_free=10G,h_vmem=10G,h_fsize=100G
+#$ -l bluejay,mem_free=10G,h_vmem=10G,h_fsize=100G
 #$ -pe local 10
 #$ -N "build_bims_NAc_genes"
 #$ -m e
@@ -25,7 +25,7 @@ module load conda_R/4.0
 module list
 
 ## Compute weights for the given region/feature pair
-Rscript build_bims.R -c 10 -t FALSE
+Rscript build_bims.R -c 10 -t TRUE
 
 echo "**** Job ends ****"
 date
