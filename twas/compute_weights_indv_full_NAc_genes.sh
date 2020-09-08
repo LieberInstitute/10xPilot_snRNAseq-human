b26e12afcc66d5eb19a9c46a82b3885cb0c11529
lcolladotor <lcollado@jhsph.edu>
2020-09-08 16:06:45 -0400

    Add the TASK ID to the logs https://github.com/LieberInstitute/twas/blob/master/bsp2/compute_weights_indv.sh#L40-L41

--- twas/compute_weights_indv_full_NAc_genes.sh
+++ twas/compute_weights_indv_full_NAc_genes.sh
@@ -2,8 +2,8 @@
 #$ -cwd
 #$ -l bluejay,mem_free=4G,h_vmem=4G,h_fsize=100G
 #$ -N compute_weights_full_NAc_genes
-#$ -o logs/NAc_genes/compute_weights_indv_full_NAc_genes.txt
-#$ -e logs/NAc_genes/compute_weights_indv_full_NAc_genes.txt
+#$ -o logs/NAc_genes/compute_weights_indv_full_NAc_genes.$TASK_ID.txt
+#$ -e logs/NAc_genes/compute_weights_indv_full_NAc_genes.$TASK_ID.txt
 #$ -t 21818,31798
 #$ -tc 40
 #$ -m e
