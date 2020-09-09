8f90de1611aa9117a13cc30f2aa9dd12007a41cd
lcolladotor <lcollado@jhsph.edu>
2020-09-09 11:45:47 -0400

    Add umask code, then run for all genes

--- twas/run_compute_weights_indv_full_NAc_genes.sh
+++ twas/run_compute_weights_indv_full_NAc_genes.sh
@@ -4,6 +4,11 @@
 ## outside the loop at
 ## https://github.com/LieberInstitute/twas/blob/master/bsp2/compute_weights_indv.sh
 
+## To avoid having to change the file permissions later
+## From https://twitter.com/fellgernon/status/1258455434073124865?s=20
+## and https://www.cyberciti.biz/tips/understanding-linux-unix-umask-value-usage.html
+umask u=rwx,g=rwx,o= ## equivalent to umask 007
+
 ## Required order for running this code:
 
 # For the logs
