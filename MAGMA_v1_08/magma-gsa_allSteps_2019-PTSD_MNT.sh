aef18a9e580fcea523309a381cea3c57daa10a70
Matt N Tran <nnguye44@jhmi.edu>
2021-05-05 23:16:10 -0400

    PTSD MAGMA-GSA w/ 'non0median' markers filter

--- MAGMA_v1_08/magma-gsa_allSteps_2019-PTSD_MNT.sh
+++ MAGMA_v1_08/magma-gsa_allSteps_2019-PTSD_MNT.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #$ -cwd
-#$ -N magma-gsa_all_PTSD_updated_MNT
-#$ -o ./logs/magma-gsa_all_PTSD_v1.08_MNT10Sep2020.o
-#$ -e ./logs/magma-gsa_all_PTSD_v1.08_MNT10Sep2020.e
+#$ -N magma-gsa_step3_PTSD-v2_MNT
+#$ -o ./logs/magma-gsa_step3-PTSD_v2_MNT05May2021.o
+#$ -e ./logs/magma-gsa_step3-PTSD_v2_MNT05May2021.e
 #$ -l bluejay,mem_free=32G,h_vmem=40G
 
 echo "**** Job starts ****"
@@ -24,17 +24,19 @@ gs_amy=/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/MAGMA/amyMa
 
 
 ## Step 1 - Annotation (SNP : gene mapping)
-$MAGMA --annotate window=35,10 --snp-loc ../MAGMA/GWAS_Results/PTSD_Nievergelt2019.snploc --gene-loc $ANNO --out SNP_Data/PTSD_Nievergelt2019_10xPilotGenes
+# $MAGMA --annotate window=35,10 --snp-loc ../MAGMA/GWAS_Results/PTSD_Nievergelt2019.snploc --gene-loc $ANNO --out SNP_Data/PTSD_Nievergelt2019_10xPilotGenes
 
 ## Step 2 - Gene analysis (from SNP-wise summary stats)
-$MAGMA --bfile $BFILE --gene-annot SNP_Data/PTSD_Nievergelt2019_10xPilotGenes.genes.annot --pval ../MAGMA/GWAS_Results/MVP.TE.REX.txt use=SNP,P ncol=N --gene-model ${model} --out SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_${model}
+# $MAGMA --bfile $BFILE --gene-annot SNP_Data/PTSD_Nievergelt2019_10xPilotGenes.genes.annot --pval ../MAGMA/GWAS_Results/MVP.TE.REX.txt use=SNP,P ncol=N --gene-model ${model} --out SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_${model}
 
 ## Step 3 - Gene set analyses (using gene-level output)  -  for five brain regions
-$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_dlpfc gene-col=${genecol} set-col=${setcol} --out Results/dlpfc_PTSD
-$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_sacc gene-col=${genecol} set-col=${setcol} --out Results/sacc_PTSD
-$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_hpc gene-col=${genecol} set-col=${setcol} --out Results/hpc_PTSD
-$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_nac gene-col=${genecol} set-col=${setcol} --out Results/nac_PTSD
-$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_amy gene-col=${genecol} set-col=${setcol} --out Results/amy_PTSD
+echo "Update MNT 05May2021 - running an iteration (into 'Results_v2') with the preprint stats, applying a filter for non-0-median expression, in each respective subcluster."
+
+$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_dlpfc gene-col=${genecol} set-col=${setcol} --out Results_v2/dlpfc_PTSD
+$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_sacc gene-col=${genecol} set-col=${setcol} --out Results_v2/sacc_PTSD
+$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_hpc gene-col=${genecol} set-col=${setcol} --out Results_v2/hpc_PTSD
+$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_nac gene-col=${genecol} set-col=${setcol} --out Results_v2/nac_PTSD
+$MAGMA --gene-results SNP_Data/PTSD_Nievergelt2019_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_amy gene-col=${genecol} set-col=${setcol} --out Results_v2/amy_PTSD
 
 
 echo "**** Job ends ****"
