39449d24e860c5c0d1e52617d15fdbf962752d65
lahuuki <lahuuki@gmail.com>
2021-05-21 11:32:20 -0400

    Add custom plotExpression wrapper to add italic x axis text + median bars

---
+++ plotExpressionCustom.R
@@ -0,0 +1,25 @@
+
+## TEST
+# test_markers <- list(A = paste0("Gene_000", 1:4),
+#                      B = paste0("Gene_000", 5:8))
+# 
+# plotExpressionCustom(sce = example_sce, features = test_markers$A, "A", anno_name = "Cell_Cycle")
+
+
+plotExpressionCustom <- function(sce, features, features_name ,anno_name = "cellType"){
+  scater::plotExpression(sce, 
+                         exprs_values = "logcounts", 
+                         features = features,
+                         x = anno_name, 
+                         colour_by = anno_name, 
+                         point_alpha = 0.2, 
+                         point_size = .7,
+                         add_legend = F) +
+  stat_summary(fun = median, 
+               fun.min = median, 
+               fun.max = median,
+               geom = "crossbar", 
+               width = 0.3)+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, face = "italic")) +  
+  ggtitle(label=paste0(features_name, " Markers"))
+}
