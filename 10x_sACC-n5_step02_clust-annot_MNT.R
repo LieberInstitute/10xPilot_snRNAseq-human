5e5959eaf2929ea33732c2df460f00ecacb06240
Matt N Tran <nnguye44@jhmi.edu>
2021-06-07 16:44:31 -0400

    sACC: finalize annotations; assign colors

--- 10x_sACC-n5_step02_clust-annot_MNT.R
+++ 10x_sACC-n5_step02_clust-annot_MNT.R
@@ -15,6 +15,8 @@ library(jaffelab)
 library(dendextend)
 library(dynamicTreeCut)
 
+source("plotExpressionCustom.R")
+
 ### Palette taken from `scater`
 tableau10medium = c("#729ECE", "#FF9E4A", "#67BF5C", "#ED665D",
                     "#AD8BC9", "#A8786E", "#ED97CA", "#A2A2A2",
@@ -101,7 +103,7 @@ save(sce.sacc, chosen.hvgs.sacc, ref.sampleInfo, ref.sampleInfo.rev,
      file="rdas/revision/regionSpecific_sACC-n5_cleaned-combined_SCE_MNT2021.rda")
 
 
-    ## 'getClusteredPCs()' evaluated in qsub mode (with 'R-batchJob_sACC-n2_optimalPCselxn_MNTFeb2020.R')
+    ## 'getClusteredPCs()' evaluated in qsub mode (with 'R-batchJob_sACC-n5_optimalPCselxn_MNT2021.R')
     #    --> saved into same .rda
 
 
@@ -157,8 +159,7 @@ save(sce.sacc, chosen.hvgs.sacc, pc.choice.sacc, ref.sampleInfo,
 
 
 
-### Step 2: Hierarchical clustering of pseudo-bulked ("PB'd") counts with most robust normalization
-  #         (as determined in: 'side-Rscript_testingStep2_HC-normalizn-approaches_wAmygData_MNTJan2020.R')
+### Step 2: Hierarchical clustering of pseudo-bulked ("PB'd") counts
 #           ** That is, to pseudo-bulk (aka 'cluster-bulk') on raw counts, on all [non-zero] genes,
 #              normalize with `librarySizeFactors()`, log2-transform, then perform HC'ing
 
@@ -169,11 +170,6 @@ prelimCluster.PBcounts <- sapply(clusIndexes, function(ii){
   rowSums(assays(sce.sacc)$counts[ ,ii])
   }
 )
-
-    # And btw...
-    table(rowSums(prelimCluster.PBcounts)==0)
-    # FALSE  TRUE
-    # 29600  3938 
 
 # Compute LSFs at this level
 sizeFactors.PB.all  <- librarySizeFactors(prelimCluster.PBcounts)
@@ -224,8 +220,13 @@ unname(clust.treeCut[order.dendrogram(dend)])
 clust.treeCut[order.dendrogram(dend)][which(
   clust.treeCut[order.dendrogram(dend)]==0)] <- max(clust.treeCut) + c(1,2, 3,3, 4:10)
 
-# Add some colors for the sake of the dendrogram
-labels_colors(dend) <- c(tableau20, tableau10medium)[clust.treeCut[order.dendrogram(dend)]]
+# ( 'Re-write', in case there are missing numbers )
+clust.treeCut[order.dendrogram(dend)] <- as.numeric(as.factor(clust.treeCut[order.dendrogram(dend)]))
+
+## Define color pallet
+cluster_colors <- unique(c(tableau10medium, tableau20)[clust.treeCut[order.dendrogram(dend)]])
+names(cluster_colors) <- unique(clust.treeCut[order.dendrogram(dend)])
+labels_colors(dend) <- cluster_colors[as.character(clust.treeCut[order.dendrogram(dend)])]
 
 # Print for future reference
 pdf("pdfs/revision/regionSpecific_sACC-n5_HC-prelimCluster-relationships_MNT2021.pdf", width=9)
@@ -265,7 +266,9 @@ markers.mathys.custom = list(
   'oligodendrocyte_precursor' = c('PDGFRA', 'VCAN', 'CSPG4'),
   'microglia' = c('CD74', 'CSF1R', 'C3'),
   'astrocytes' = c('GFAP', 'TNC', 'AQP4', 'SLC1A2'),
-  'endothelial' = c('CLDN5', 'FLT1', 'VTN', 'PECAM1')
+  'endothelial' = c('CLDN5', 'FLT1', 'VTN', 'PECAM1'),
+  # T cell markers - these seem to be somewhat common (across regions)
+  'Tcell' = c('SKAP1', 'ITK', 'CD247')
 )
 
 pdf("pdfs/revision/regionSpecific_sACC-n5_marker-logExprs_collapsedClusters_MNT2021.pdf", height=6, width=10)
@@ -351,7 +354,7 @@ annotationTab.sacc$cellType[18] <- "drop.doublet"  # ahhh, these are doublets (s
 annotationTab.sacc$cellType[c(16,19)] <- paste0("Astro_", c("A","B"))
 annotationTab.sacc$cellType[20] <- "Micro"
 annotationTab.sacc$cellType[22] <- "drop.lowNTx"
-annotationTab.sacc$cellType[24] <- "ambig.SYT1.SNAP25"
+annotationTab.sacc$cellType[24] <- "Neu_FAT2.CDH15"
     # perhaps this one is axonal debris??  It was prelimCluster 67 (stuck out a ton)
 
 
@@ -360,8 +363,24 @@ sce.sacc$cellType <- annotationTab.sacc$cellType[match(sce.sacc$collapsedCluster
                                                        annotationTab.sacc$collapsedCluster)]
 sce.sacc$cellType <- factor(sce.sacc$cellType)
 
+# Set colors to be stable for this region/these cell classes
+cell_colors.sacc <- cluster_colors[order(as.integer(names(cluster_colors)))]
+names(cell_colors.sacc) <- annotationTab.sacc$cellType
+cell_colors.sacc
+    #   Inhib_A        Inhib_B        Inhib_C        Excit_A        Excit_B        Inhib_D 
+    # "#729ECE"      "#FF9E4A"      "#67BF5C"      "#ED665D"      "#AD8BC9"      "#A8786E" 
+    #   Inhib_E        Excit_C        Excit_D        Inhib_F        Inhib_G        Oligo_A 
+    # "#ED97CA"      "#A2A2A2"      "#CDCC5D"      "#6DCCDA"      "#1F77B4"      "#AEC7E8" 
+    #   Excit_E        Inhib_H        Excit_F        Astro_A            OPC   drop.doublet 
+    # "#FF7F0E"      "#FFBB78"      "#2CA02C"      "#98DF8A"      "#D62728"      "#FF9896" 
+    #   Astro_B          Micro        Oligo_B    drop.lowNTx        Excit_G Neu_FAT2.CDH15 
+    # "#9467BD"      "#C5B0D5"      "#8C564B"      "#C49C94"      "#E377C2"      "#F7B6D2" 
+    #   Inhib_I        Inhib_J        Inhib_K 
+    # "#7F7F7F"      "#C7C7C7"      "#BCBD22"
+
+
 ## Save
-save(sce.sacc, chosen.hvgs.sacc, pc.choice.sacc, clusterRefTab.sacc, ref.sampleInfo, annotationTab.sacc,
+save(sce.sacc, chosen.hvgs.sacc, pc.choice.sacc, clusterRefTab.sacc, ref.sampleInfo, annotationTab.sacc, cell_colors.sacc,
      file="/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/revision/regionSpecific_sACC-n5_cleaned-combined_SCE_MNT2021.rda")
 
 
@@ -370,14 +389,14 @@ save(sce.sacc, chosen.hvgs.sacc, pc.choice.sacc, clusterRefTab.sacc, ref.sampleI
 # load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/revision/regionSpecific_sACC-n5_cleaned-combined_SCE_MNT2021.rda",
 #      verbose=T)
 
-pdf("pdfs/revision/regionSpecific_sACC-n5_marker-logExprs_collapsedClusters_MNT2021.pdf", height=6, width=10)
+pdf("pdfs/revision/regionSpecific_sACC-n5_marker-logExprs_collapsedClusters_MNT2021.pdf", height=4.5, width=10)
 for(i in 1:length(markers.mathys.custom)){
   print(
-    plotExpression(sce.sacc, exprs_values = "logcounts", features=c(markers.mathys.custom[[i]]),
-                   x="cellType", colour_by="cellType", point_alpha=0.5, point_size=.7,
-                   add_legend=F, show_median=T) +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1)) +  
-      ggtitle(label=paste0(names(markers.mathys.custom)[i], " markers"))
+    plotExpressionCustom(sce = sce.sacc,
+                         features = markers.mathys.custom[[i]], 
+                         features_name = names(markers.mathys.custom)[[i]], 
+                         anno_name = "cellType") +
+      scale_color_manual(values = cell_colors.sacc)
   )
 }
 dev.off()
@@ -393,21 +412,28 @@ dev.off()
 
     
 ## Re-print reducedDims with these annotations ===
-pdf("pdfs/revision/regionSpecific_sACC-n5_reducedDims-with-collapsedClusters_MNT2021.pdf")
-plotReducedDim(sce.sacc, dimred="PCA_corrected", ncomponents=5, colour_by="cellType", point_alpha=0.5)
-plotTSNE(sce.sacc, colour_by="sampleID", point_alpha=0.5)
-plotTSNE(sce.sacc, colour_by="protocol", point_alpha=0.5)
+pdf("pdfs/revision/regionSpecific_sACC-n5_reducedDims-with-collapsedClusters_MNT2021.pdf", width=8)
+plotReducedDim(sce.sacc, dimred="PCA_corrected", ncomponents=5, colour_by="cellType", point_alpha=0.5) +
+  scale_color_manual(values = cell_colors.sacc) + labs(colour="Cell type")
+plotTSNE(sce.sacc, colour_by="sampleID", point_alpha=0.5, point_size=2)
+plotTSNE(sce.sacc, colour_by="protocol", point_alpha=0.5, point_size=2)
 plotTSNE(sce.sacc, colour_by="prelimCluster", text_by="prelimCluster",
-         text_size=3, point_alpha=0.5)
+         text_size=3, point_alpha=0.5, point_size=2)
 plotTSNE(sce.sacc, colour_by="cellType", text_by="cellType",
-         text_size=3, point_alpha=0.5)
-plotTSNE(sce.sacc, colour_by="sum", point_alpha=0.5)
-plotTSNE(sce.sacc, colour_by="doubletScore", point_alpha=0.5)
+         text_size=3, point_alpha=0.5, point_size=2) +
+  scale_color_manual(values = cell_colors.sacc,
+                     labels=paste0(levels(sce.sacc$cellType)," (",table(sce.sacc$cellType),")")) +
+  labs(colour="Cell type")
+plotTSNE(sce.sacc, colour_by="sum", point_alpha=0.5, point_size=2)
+plotTSNE(sce.sacc, colour_by="doubletScore", point_alpha=0.5, point_size=2)
 # And some more informative UMAPs
 plotUMAP(sce.sacc, colour_by="prelimCluster", text_by="prelimCluster",
-         text_size=3, point_alpha=0.5)
+         text_size=3, point_alpha=0.5, point_size=2)
 plotUMAP(sce.sacc, colour_by="cellType", text_by="cellType",
-         text_size=3, point_alpha=0.5)
+         text_size=3, point_alpha=0.5, point_size=2) +
+  scale_color_manual(values = cell_colors.sacc,
+                     labels=paste0(levels(sce.sacc$cellType)," (",table(sce.sacc$cellType),")")) +
+  labs(colour="Cell type")
 dev.off()
 
 
@@ -417,7 +443,7 @@ dev.off()
 
 
 
-### Session info for 29Apr2021 ==========================================================
+### Session info for 06Jun2021 ==========================================================
 sessionInfo()
 # R version 4.0.4 RC (2021-02-08 r79975)
 # Platform: x86_64-pc-linux-gnu (64-bit)
@@ -428,82 +454,58 @@ sessionInfo()
 # LAPACK: /jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.0.x/R/4.0.x/lib64/R/lib/libRlapack.so
 # 
 # locale:
-#   [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
-# [3] LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
-# [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
-# [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                 
-# [9] LC_ADDRESS=C               LC_TELEPHONE=C            
-# [11] LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+#   [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8       
+# [4] LC_COLLATE=en_US.UTF-8     LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8   
+# [7] LC_PAPER=en_US.UTF-8       LC_NAME=C                  LC_ADDRESS=C              
+# [10] LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
 # 
 # attached base packages:
-#   [1] parallel  stats4    stats     graphics  grDevices datasets  utils    
-# [8] methods   base     
+#   [1] parallel  stats4    stats     graphics  grDevices datasets  utils     methods  
+# [9] base     
 # 
 # other attached packages:
-#   [1] dynamicTreeCut_1.63-1       dendextend_1.14.0          
-# [3] jaffelab_0.99.30            rafalib_1.0.0              
-# [5] DropletUtils_1.10.3         batchelor_1.6.2            
-# [7] scran_1.18.5                scater_1.18.6              
-# [9] ggplot2_3.3.3               EnsDb.Hsapiens.v86_2.99.0  
-# [11] ensembldb_2.14.1            AnnotationFilter_1.14.0    
-# [13] GenomicFeatures_1.42.3      AnnotationDbi_1.52.0       
-# [15] SingleCellExperiment_1.12.0 SummarizedExperiment_1.20.0
-# [17] Biobase_2.50.0              GenomicRanges_1.42.0       
-# [19] GenomeInfoDb_1.26.7         IRanges_2.24.1             
-# [21] S4Vectors_0.28.1            BiocGenerics_0.36.1        
-# [23] MatrixGenerics_1.2.1        matrixStats_0.58.0         
+#   [1] dynamicTreeCut_1.63-1       dendextend_1.14.0           jaffelab_0.99.30           
+# [4] rafalib_1.0.0               DropletUtils_1.10.3         batchelor_1.6.2            
+# [7] scran_1.18.5                scater_1.18.6               ggplot2_3.3.3              
+# [10] EnsDb.Hsapiens.v86_2.99.0   ensembldb_2.14.1            AnnotationFilter_1.14.0    
+# [13] GenomicFeatures_1.42.3      AnnotationDbi_1.52.0        SingleCellExperiment_1.12.0
+# [16] SummarizedExperiment_1.20.0 Biobase_2.50.0              GenomicRanges_1.42.0       
+# [19] GenomeInfoDb_1.26.7         IRanges_2.24.1              S4Vectors_0.28.1           
+# [22] BiocGenerics_0.36.1         MatrixGenerics_1.2.1        matrixStats_0.58.0         
 # 
 # loaded via a namespace (and not attached):
-#   [1] googledrive_1.0.1         ggbeeswarm_0.6.0         
-# [3] colorspace_2.0-0          ellipsis_0.3.1           
-# [5] scuttle_1.0.4             bluster_1.0.0            
-# [7] XVector_0.30.0            BiocNeighbors_1.8.2      
-# [9] rstudioapi_0.13           bit64_4.0.5              
-# [11] fansi_0.4.2               xml2_1.3.2               
-# [13] splines_4.0.4             R.methodsS3_1.8.1        
-# [15] sparseMatrixStats_1.2.1   cachem_1.0.4             
-# [17] Rsamtools_2.6.0           ResidualMatrix_1.0.0     
-# [19] dbplyr_2.1.1              R.oo_1.24.0              
-# [21] HDF5Array_1.18.1          compiler_4.0.4           
-# [23] httr_1.4.2                dqrng_0.2.1              
-# [25] assertthat_0.2.1          Matrix_1.3-2             
-# [27] fastmap_1.1.0             lazyeval_0.2.2           
-# [29] limma_3.46.0              BiocSingular_1.6.0       
-# [31] prettyunits_1.1.1         tools_4.0.4              
-# [33] rsvd_1.0.3                igraph_1.2.6             
-# [35] gtable_0.3.0              glue_1.4.2               
-# [37] GenomeInfoDbData_1.2.4    dplyr_1.0.5              
-# [39] rappdirs_0.3.3            Rcpp_1.0.6               
-# [41] vctrs_0.3.6               Biostrings_2.58.0        
-# [43] rhdf5filters_1.2.0        rtracklayer_1.50.0       
-# [45] DelayedMatrixStats_1.12.3 stringr_1.4.0            
-# [47] beachmat_2.6.4            lifecycle_1.0.0          
-# [49] irlba_2.3.3               statmod_1.4.35           
-# [51] XML_3.99-0.6              edgeR_3.32.1             
-# [53] zlibbioc_1.36.0           scales_1.1.1             
-# [55] hms_1.0.0                 ProtGenerics_1.22.0      
-# [57] rhdf5_2.34.0              RColorBrewer_1.1-2       
-# [59] curl_4.3                  memoise_2.0.0            
-# [61] gridExtra_2.3             segmented_1.3-3          
-# [63] biomaRt_2.46.3            stringi_1.5.3            
-# [65] RSQLite_2.2.7             BiocParallel_1.24.1      
-# [67] rlang_0.4.10              pkgconfig_2.0.3          
-# [69] bitops_1.0-7              lattice_0.20-41          
-# [71] purrr_0.3.4               Rhdf5lib_1.12.1          
-# [73] GenomicAlignments_1.26.0  bit_4.0.4                
-# [75] tidyselect_1.1.0          magrittr_2.0.1           
-# [77] R6_2.5.0                  generics_0.1.0           
-# [79] DelayedArray_0.16.3       DBI_1.1.1                
-# [81] pillar_1.6.0              withr_2.4.2              
-# [83] RCurl_1.98-1.3            tibble_3.1.1             
-# [85] crayon_1.4.1              utf8_1.2.1               
-# [87] BiocFileCache_1.14.0      viridis_0.6.0            
-# [89] progress_1.2.2            locfit_1.5-9.4           
-# [91] grid_4.0.4                blob_1.2.1               
-# [93] R.utils_2.10.1            openssl_1.4.3            
-# [95] munsell_0.5.0             beeswarm_0.3.1           
-# [97] viridisLite_0.4.0         vipor_0.4.5              
-# [99] askpass_1.1  
-
+#   [1] googledrive_1.0.1         ggbeeswarm_0.6.0          colorspace_2.0-0         
+# [4] ellipsis_0.3.2            scuttle_1.0.4             bluster_1.0.0            
+# [7] XVector_0.30.0            BiocNeighbors_1.8.2       rstudioapi_0.13          
+# [10] bit64_4.0.5               fansi_0.4.2               xml2_1.3.2               
+# [13] splines_4.0.4             R.methodsS3_1.8.1         sparseMatrixStats_1.2.1  
+# [16] cachem_1.0.4              Rsamtools_2.6.0           ResidualMatrix_1.0.0     
+# [19] dbplyr_2.1.1              R.oo_1.24.0               HDF5Array_1.18.1         
+# [22] compiler_4.0.4            httr_1.4.2                dqrng_0.2.1              
+# [25] assertthat_0.2.1          Matrix_1.3-2              fastmap_1.1.0            
+# [28] lazyeval_0.2.2            limma_3.46.0              BiocSingular_1.6.0       
+# [31] prettyunits_1.1.1         tools_4.0.4               rsvd_1.0.3               
+# [34] igraph_1.2.6              gtable_0.3.0              glue_1.4.2               
+# [37] GenomeInfoDbData_1.2.4    dplyr_1.0.5               rappdirs_0.3.3           
+# [40] Rcpp_1.0.6                vctrs_0.3.6               Biostrings_2.58.0        
+# [43] rhdf5filters_1.2.0        rtracklayer_1.50.0        DelayedMatrixStats_1.12.3
+# [46] stringr_1.4.0             beachmat_2.6.4            lifecycle_1.0.0          
+# [49] irlba_2.3.3               statmod_1.4.35            XML_3.99-0.6             
+# [52] edgeR_3.32.1              zlibbioc_1.36.0           scales_1.1.1             
+# [55] hms_1.0.0                 ProtGenerics_1.22.0       rhdf5_2.34.0             
+# [58] RColorBrewer_1.1-2        curl_4.3                  memoise_2.0.0            
+# [61] gridExtra_2.3             segmented_1.3-3           biomaRt_2.46.3           
+# [64] stringi_1.5.3             RSQLite_2.2.7             BiocParallel_1.24.1      
+# [67] rlang_0.4.10              pkgconfig_2.0.3           bitops_1.0-7             
+# [70] lattice_0.20-41           purrr_0.3.4               Rhdf5lib_1.12.1          
+# [73] GenomicAlignments_1.26.0  bit_4.0.4                 tidyselect_1.1.1         
+# [76] magrittr_2.0.1            R6_2.5.0                  generics_0.1.0           
+# [79] DelayedArray_0.16.3       DBI_1.1.1                 pillar_1.6.0             
+# [82] withr_2.4.2               RCurl_1.98-1.3            tibble_3.1.1             
+# [85] crayon_1.4.1              utf8_1.2.1                BiocFileCache_1.14.0     
+# [88] viridis_0.6.0             progress_1.2.2            locfit_1.5-9.4           
+# [91] grid_4.0.4                blob_1.2.1                R.utils_2.10.1           
+# [94] openssl_1.4.3             munsell_0.5.0             beeswarm_0.3.1           
+# [97] viridisLite_0.4.0         vipor_0.4.5               askpass_1.1 
 
 
