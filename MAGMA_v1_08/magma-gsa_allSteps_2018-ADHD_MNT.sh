4ebdb2aec6947f9b9f95b694838e8f369290d42f
Matt N Tran <nnguye44@jhmi.edu>
2021-06-22 13:43:50 -0400

    MAGMA-gene set analysis with preprint subpops: 2018 ADHD GWAS

--- MAGMA_v1_08/magma-gsa_allSteps_2018-ADHD_MNT.sh
+++ MAGMA_v1_08/magma-gsa_allSteps_2018-ADHD_MNT.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #$ -cwd
-#$ -N magma-gsa_step1-2_ADHD
-#$ -o ./logs/magma-gsa_step1-2-ADHD_MNT22Jun2021.o
-#$ -e ./logs/magma-gsa_step1-2-ADHD_MNT22Jun2021.e
-#$ -l bluejay,mem_free=32G,h_vmem=40G
+#$ -N magma-gsa_step3_ADHD
+#$ -o ./logs/magma-gsa_step3-ADHD_MNT22Jun2021.o
+#$ -e ./logs/magma-gsa_step3-ADHD_MNT22Jun2021.e
+#$ -l bluejay,mem_free=16G,h_vmem=24G
 
 echo "**** Job starts ****"
 date
@@ -25,19 +25,20 @@ gs_amy=/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/MAGMA/amyMa
 SUMMSTATS=/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/MAGMA/GWAS_Results/daner_adhd_meta_filtered_NA_iPSYCH23_PGC11_sigPCs_woSEX_2ell6sd_EUR_Neff_70.meta
 
 ## Step 1 - Annotation (SNP : gene mapping)
-$MAGMA --annotate window=35,10 --snp-loc ../MAGMA/GWAS_Results/ADHD_PGC2018.snploc --gene-loc $ANNO --out SNP_Data/ADHD_PGC2018_10xPilotGenes
+# $MAGMA --annotate window=35,10 --snp-loc ../MAGMA/GWAS_Results/ADHD_PGC2018.snploc --gene-loc $ANNO --out SNP_Data/ADHD_PGC2018_10xPilotGenes
 
 ## Step 2 - Gene analysis (from SNP-wise summary stats)
-$MAGMA --bfile $BFILE --gene-annot SNP_Data/ADHD_PGC2018_10xPilotGenes.genes.annot --pval $SUMMSTATS use=SNP,P ncol=Neff --gene-model ${model} --out SNP_Data/ADHD_PGC2018_10xPilotGenes_${model}
+# $MAGMA --bfile $BFILE --gene-annot SNP_Data/ADHD_PGC2018_10xPilotGenes.genes.annot --pval $SUMMSTATS use=SNP,P ncol=Neff --gene-model ${model} --out SNP_Data/ADHD_PGC2018_10xPilotGenes_${model}
 
 
 ## Step 3 - Gene set analyses (using gene-level output)  -  for five brain regions
+echo "MNT comment 22Jun2021 - running an iteration (into 'Results_v2') with the preprint stats, applying a filter for non-0-median expression, in each respective subcluster. \n This is being done after prepint; only running to see what result looks like with reported preprint cell type subpopulations."
 
-# $MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_dlpfc gene-col=${genecol} set-col=${setcol} --out Results_v2/dlpfc_ADHD
-# $MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_sacc gene-col=${genecol} set-col=${setcol} --out Results_v2/sacc_ADHD
-# $MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_hpc gene-col=${genecol} set-col=${setcol} --out Results_v2/hpc_ADHD
-# $MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_nac gene-col=${genecol} set-col=${setcol} --out Results_v2/nac_ADHD
-# $MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_amy gene-col=${genecol} set-col=${setcol} --out Results_v2/amy_ADHD
+$MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_dlpfc gene-col=${genecol} set-col=${setcol} --out Results_v2/dlpfc_ADHD
+$MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_sacc gene-col=${genecol} set-col=${setcol} --out Results_v2/sacc_ADHD
+$MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_hpc gene-col=${genecol} set-col=${setcol} --out Results_v2/hpc_ADHD
+$MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_nac gene-col=${genecol} set-col=${setcol} --out Results_v2/nac_ADHD
+$MAGMA --gene-results SNP_Data/ADHD_PGC2018_10xPilotGenes_snp-wise.genes.raw --set-annot $gs_amy gene-col=${genecol} set-col=${setcol} --out Results_v2/amy_ADHD
 
 
 echo "**** Job ends ****"
