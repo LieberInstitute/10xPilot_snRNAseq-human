407cf7db4c48ae4c824dd26c5b2b0a1260333d4f
Matt N Tran <nnguye44@jhmi.edu>
2021-06-23 13:05:47 -0400

    NAc: getClusteredPCs in R batch job

--- R-batchJob_NAc-n8_optimalPCselxn_MNT2021.R
+++ R-batchJob_NAc-n8_optimalPCselxn_MNT2021.R
@@ -1,10 +1,10 @@
 ################################################################################
-### LIBD pilot 10x snRNA-seq: Nucleus accumbens samples - ALL FIVE
+### LIBD pilot 10x snRNA-seq: Nucleus accumbens samples - 8 samples (as of revision)
 ###   **Region-specific analyses**
-###   - R-batch job for detxn of optimal PC space with 'sce.nac.all' object
-###         -> see '10x-pilot_region-specific_NAc-n3_with_NeuN-sorted-n2_step02_clust-annot_MNTMar2020.R'
+###   - R-batch job for detxn of optimal PC space with 'sce.nac' object
+###         -> see '10x_NAc-n8_step02_clust-annot_MNT.R'
 ###            for setup of the SCE
-### MNT 23Mar2020
+### MNT 23Jun2021
 ################################################################################
 
 library(SingleCellExperiment)
@@ -16,38 +16,34 @@ library(scran)
 library(uwot)
 library(DropletUtils)
 library(jaffelab)
-library(Rtsne)
 
 # ===
 
 
-load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/NeuN-sortedNAc_n2_cleaned-combined_MNTMar2020.rda",
+load("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/revision/regionSpecific_NAc-n8_cleaned-combined_MNT2021.rda",
      verbose=TRUE)
-    # sce.nac.all, chosen.hvgs.nac.all
+    # sce.nac, chosen.hvgs.nac, ref.sampleInfo
 
-sce.nac.all
-table(sce.nac.all$sample)   # 5 samples
-dim(reducedDim(sce.nac.all, "PCA"))
-    ## PCA already done (interactively) - took top 100 PCs
+## ** [corrected] PCA already done (interactively, using `fastMNN`) - took top 100 PCs **
 
-## getClusteredPCs() to identify working PC space
-pc.choice.nac.all <- getClusteredPCs(reducedDim(sce.nac.all))
+# getClusteredPCs() to identify working PC space
+pc.choice.nac <- getClusteredPCs(reducedDim(sce.nac, "PCA_corrected"))
 
 # How many PCs should use in this space?
-metadata(pc.choice.nac.all)$chosen
+metadata(pc.choice.nac)$chosen
 
 
-    ## Plot n Clusters vs. d PCs
-    pdf("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/pdfs/regionSpecific_NAc-ALL-n5_getClusteredPCs-results-w100pcs_MNTMar2020.pdf")
-    plot(pc.choice.nac.all$n.pcs, pc.choice.nac.all$n.clusters,
-         main=paste0("Combined NAc (n=3 homs, n=2 NeuN-sorts) samples (d PCs choice = ", metadata(pc.choice.nac.all)$chosen, ")"))
-    abline(v=metadata(pc.choice.nac.all)$chosen, col="red", lty="dashed", lwd=0.8)  
-    dev.off()
+## Plot n Clusters vs. d PCs
+pdf("/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/pdfs/revision/regionSpecific_NAc-n8_getClusteredPCs_MNT2021.pdf")
+plot(pc.choice.nac$n.pcs, pc.choice.nac$n.clusters,
+     main=paste0("Combined NAc (n=8) samples (d PCs choice = ", metadata(pc.choice.nac)$chosen, ")"))
+abline(v=metadata(pc.choice.nac)$chosen, col="red", lty="dashed", lwd=0.8)  
+dev.off()
 
 
 # Save
-save(sce.nac.all, chosen.hvgs.nac.all, pc.choice.nac.all,
-     file="/dcl01/lieber/ajaffe/Matt/MNT_thesis/snRNAseq/10x_pilot_FINAL/rdas/regionSpecific_NAc-ALL-n5_cleaned-combined_SCE_MNTMar2020.rda")
+save(sce.nac, chosen.hvgs.nac, ref.sampleInfo, pc.choice.nac,
+     file="rdas/revision/regionSpecific_NAc-n8_cleaned-combined_MNT2021.rda")
 
 rm(list=ls())
 sessionInfo()
