4d1487128b35f0f2c0d42e2d8de621a3be25317a
Matt N Tran <nnguye44@jhmi.edu>
2021-07-02 07:54:21 -0400

    stop calling 'no-low-N-Txts' since this is QC

--- 10x_miscPUBLICATIONstuff_figures-metrics_MNT.R
+++ 10x_miscPUBLICATIONstuff_figures-metrics_MNT.R
@@ -336,113 +336,38 @@ quantile(as.numeric(gsub(",","",n14.metrics.collapsed$Number.of.Reads)))
 
 
 ## NAc, all n=5 ===
-load("rdas/regionSpecific_NAc-ALL-n5_cleaned-combined_SCE_MNTMar2020.rda", verbose=T)
-#sce.nac.all, chosen.hvgs.nac.all, pc.choice.nac.all, clusterRefTab.nac.all, ref.sampleInfo
+load("rdas/revision/regionSpecific_NAc-n8_cleaned-combined_MNT2021.rda", verbose=T)
+    # sce.nac, chosen.hvgs.nac, pc.choice.nac, ref.sampleInfo, annotationTab.nac, cell_colors.nac
 
 ## Distribution of n transcripts
 # 'Depth' per sample
-nac.samples <- splitit(sce.nac.all$sample)
-sapply(nac.samples, function(x){quantile(sce.nac.all[ ,x]$sum)})
+nac.samples <- splitit(sce.nac$sample)
+sapply(nac.samples, function(x){quantile(sce.nac[ ,x]$sum)})
 # 'Capture' per cell type
-cellType_idx <- splitit(sce.nac.all$cellType)
-sapply(cellType_idx, function(x){quantile(sce.nac.all[ ,x]$sum)})
+cellType_idx <- splitit(sce.nac$cellType)
+sapply(cellType_idx, function(x){quantile(sce.nac[ ,x]$sum)})
 # Median n UMIs per nucleus
-quantile(sce.nac.all$sum, probs=seq(0.1,1,by=0.1))
+quantile(sce.nac$sum, probs=seq(0.1,1,by=0.1))
 #   10%   20%   30%   40%   50%   60%   70%   80%   90%  100%
 #  4472  7308 11360 16017 19249 21819 24411 27653 32519 85460
 
 # n Nuclei from each sample
-table(sce.nac.all$sample)
+table(sce.nac$sample)
 # nac.5161      nac.5212      nac.5287 nac.neun.5182 nac.neun.5207
 #     2067          1774           707          4267          4426
 
-signif(sum(assay(sce.nac.all, "counts")),3)
+signif(sum(assay(sce.nac, "counts")),3)
 #[1] 2.51e+08
 
-## pseudoSort - did this add noise? - look at marker genes ==============
-# (first load .rda)
-sce.nac.all <- sce.nac.all[ ,!sce.nac.all$cellType=="ambig.lowNtrxts"]
-sce.nac.all$cellType.final <- droplevels(sce.nac.all$cellType.final)
-
-# Make broad MSN1/MSN2/inhib and collapse true sorts
-sce.nac.all$cellType.final <- as.character(sce.nac.all$cellType.final)
-sce.nac.all$cellType.final[grep("D1", sce.nac.all$cellType.final)] <- "MSN.D1"
-sce.nac.all$cellType.final[grep("D2", sce.nac.all$cellType.final)] <- "MSN.D2"
-sce.nac.all$cellType.final[grep("Inhib.", sce.nac.all$cellType.final)] <- "Inhib"
-
-
-sce.nac.all$protocol <- gsub("Frank.NeuN", "Frank",sce.nac.all$protocol)
-
-
-plotExpression(sce.nac.all, exprs_values = "logcounts", features=c("MBP", "AQP4"),
-               x="cellType.final", colour_by="cellType.final", point_alpha=0.5, point_size=.7, ncol=5,
-               add_legend=F) + stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,
-                                            geom = "crossbar", width = 0.3) +
-  #facet_wrap(~ protocol) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
-
-table(sce.nac.all$cellType.final, sce.nac.all$protocol)
-#        Frank pseudoSort
-# Astro     12        533
-# Inhib    503         37
-# Micro     37        144
-# MSN.D1  4518        387
-# MSN.D2  3762        169
-# Oligo    499       2308
-# OPC       37        202
-
-# Paste cluster:protocol
-sce.nac.all$clustProt <- paste0(sce.nac.all$cellType.final, ".",sce.nac.all$protocol)
-
-plotExpression(sce.nac.all, exprs_values = "logcounts", features=c("MBP", "AQP4"),
-               x="clustProt", colour_by="clustProt", point_alpha=0.5, point_size=.7, ncol=5,
-               add_legend=F) + stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,
-                                            geom = "crossbar", width = 0.3,
-                                            colour=rep(tableau20[1:14], 2)) +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
-
-
-# IQRs across broad markers?
-clustProt.idx <- splitit(sce.nac.all$clustProt)
-
-sapply(c("MBP","MOBP","PLP1",
-         "PDGFRA", "VCAN",
-         "C3", "CD74",
-         "AQP4",
-         "SNAP25", "SYT1",
-         "DRD1", "DRD2"), function(g){
-           sapply(clustProt.idx, function(x){signif(iqr(assay(sce.nac.all,"logcounts")[g,x]),3)})
-         }
-)
-#                     MBP  MOBP  PLP1 PDGFRA  VCAN    C3  CD74 AQP4 SNAP25  SYT1 DRD1  DRD2
-# Astro.Frank       0.264 0.000 1.050   0.00 3.150 0.000 0.000 1.33  1.040 0.186 0.00 0.000
-# Astro.pseudoSort  0.000 0.000 0.000   0.00 1.340 0.000 0.000 1.14  0.000 0.000 0.00 0.000
-# Inhib.Frank       2.120 0.000 0.000   0.00 0.000 0.000 0.000 0.00  0.837 6.410 0.00 1.190
-# Inhib.pseudoSort  1.940 0.000 0.000   0.00 0.000 0.000 0.000 0.00  0.629 6.210 0.00 0.911
-# Micro.Frank       1.600 0.000 1.730   0.00 0.000 0.995 0.884 0.00  0.000 0.000 0.00 0.000
-# Micro.pseudoSort  3.220 0.000 0.000   0.00 0.000 1.820 2.080 0.00  0.000 0.000 0.00 0.000
-# MSN.D1.Frank      0.714 0.000 0.000   0.00 0.000 0.000 0.000 0.00  0.718 0.526 1.59 0.000
-# MSN.D1.pseudoSort 0.597 0.539 0.467   0.00 0.000 0.000 0.000 0.00  0.823 0.578 1.18 0.576
-# MSN.D2.Frank      0.000 0.444 0.000   0.00 0.000 0.000 0.000 0.00  0.700 0.508 0.00 0.855
-# MSN.D2.pseudoSort 0.000 0.396 0.367   0.00 0.000 0.000 0.000 0.00  0.676 0.719 0.00 0.844
-# Oligo.Frank       0.766 1.410 0.657   0.00 0.000 0.000 0.000 0.00  0.000 0.000 0.00 0.000
-# Oligo.pseudoSort  0.833 1.790 1.170   0.00 0.000 0.000 0.000 0.00  0.000 0.000 0.00 0.000
-# OPC.Frank         1.090 0.000 1.750   1.53 0.726 0.000 0.000 0.00  2.370 0.000 0.00 0.000
-# OPC.pseudoSort    2.790 0.000 0.000   2.47 1.120 0.000 0.000 0.00  2.750 0.000 0.00 0.000
-
-#     Interpretation: basically, there's more spread in the pseudosort for at least
-#                     the glial populations with strong numbers in both 'protocols'
-#                     and their respective marker genes
-# ==========
 
 
 # Make diff object for 'ambig.lowNtrxts' removed
-sce.nac.noLNT <- sce.nac.all[ ,!sce.nac.all$cellType=="ambig.lowNtrxts"]
-sce.nac.noLNT$cellType.final <- droplevels(sce.nac.noLNT$cellType.final)
+sce.nac <- sce.nac[ ,!sce.nac$cellType=="ambig.lowNtrxts"]
+sce.nac$cellType.final <- droplevels(sce.nac$cellType.final)
 
 ## Proportion of cell types
-table(sce.nac.noLNT$cellType.final)
-prop.table(table(sce.nac.noLNT$cellType.final))
+table(sce.nac$cellType.final)
+prop.table(table(sce.nac$cellType.final))
 #       Astro     Inhib.1     Inhib.2     Inhib.3     Inhib.4       Micro
 # 0.041451171 0.001901430 0.004259203 0.020991786 0.013918467 0.013766352
 #    MSN.D1.1    MSN.D1.2    MSN.D1.3    MSN.D1.4    MSN.D2.1    MSN.D2.2
@@ -451,11 +376,11 @@ prop.table(table(sce.nac.noLNT$cellType.final))
 # 0.213492546 0.018177670
 
 # How many MSNs in our dataset?
-sum(prop.table(table(sce.nac.noLNT$cellType.final))[grep("MSN", levels(sce.nac.noLNT$cellType.final))])
+sum(prop.table(table(sce.nac$cellType.final))[grep("MSN", levels(sce.nac$cellType.final))])
 # However this is including two samples where we enriched on neurons
 
 
-table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$sample)
+table(sce.nac$cellType.final, sce.nac$sample)
 # 
 #          nac.5161 nac.5212 nac.5287 nac.neun.5182 nac.neun.5207
 # Astro         149      384       12             0             0
@@ -474,7 +399,7 @@ table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$sample)
 # OPC            98      104       37             0             0
 
 ## Proportions of cell type by sample
-apply(table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$sample), 2, function(x){round(prop.table(x),3)})
+apply(table(sce.nac$cellType.final, sce.nac$sample), 2, function(x){round(prop.table(x),3)})
 #          nac.5161 nac.5212 nac.5287 nac.neun.5182 nac.neun.5207
 # Astro       0.073    0.222    0.018         0.000         0.000
 # Inhib.1     0.000    0.002    0.000         0.004         0.001
@@ -493,22 +418,22 @@ apply(table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$sample), 2, function(x){
 
 # Of neurons, how many are MSNs?  Let's just look at the NeuN-sorted samples
 
-table(ss(as.character(sce.nac.noLNT$cellType.final[grep("neun", sce.nac.noLNT$sample)]),
+table(ss(as.character(sce.nac$cellType.final[grep("neun", sce.nac$sample)]),
          "\\.",1),
-      sce.nac.noLNT$sample[grep("neun", sce.nac.noLNT$sample)])
+      sce.nac$sample[grep("neun", sce.nac$sample)])
 #        nac.neun.5182 nac.neun.5207
 #  Inhib           248           241
 #  MSN            4012          4182
-apply(table(ss(as.character(sce.nac.noLNT$cellType.final[grep("neun", sce.nac.noLNT$sample)]),
+apply(table(ss(as.character(sce.nac$cellType.final[grep("neun", sce.nac$sample)]),
                "\\.",1),
-            sce.nac.noLNT$sample[grep("neun", sce.nac.noLNT$sample)]),2,prop.table)
+            sce.nac$sample[grep("neun", sce.nac$sample)]),2,prop.table)
 #        nac.neun.5182 nac.neun.5207
 #  Inhib    0.05821596     0.0544879
 #  MSN      0.94178404     0.9455121
 
 # Proportions of MSN subpopulations across donors
-round(apply(prop.table(table(sce.nac.noLNT$cellType.final[grep("MSN", sce.nac.noLNT$cellType.final)],
-                             sce.nac.noLNT$sample[grep("MSN", sce.nac.noLNT$cellType.final)])),2,prop.table),3)
+round(apply(prop.table(table(sce.nac$cellType.final[grep("MSN", sce.nac$cellType.final)],
+                             sce.nac$sample[grep("MSN", sce.nac$cellType.final)])),2,prop.table),3)
 #          nac.5161 nac.5212 nac.5287 nac.neun.5182 nac.neun.5207
 # Astro       0.000    0.000    0.000         0.000         0.000
 # Inhib.1     0.000    0.000    0.000         0.000         0.000
@@ -539,36 +464,36 @@ sapply(markers.nac.t.design, function(x){sum(x$FDR < 0.05)})
 
 
 ## Are DRD1/DRD2 ever expressed together? ===
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="cellType.final", point_alpha=0.5, point_size=.7, ncol=5,
                add_legend=F) + stat_summary(fun.y = median, fun.ymin = median, fun.ymax = median,
                                             geom = "crossbar", width = 0.3,
                                             colour=rep(tableau20[1:14], 2)) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
 
-geneExprs <- assay(sce.nac.noLNT, "logcounts")
+geneExprs <- assay(sce.nac, "logcounts")
 
-sce.nac.noLNT$MSN.D1 <- NA
-sce.nac.noLNT$MSN.D1[grep("MSN.D1", sce.nac.noLNT$cellType.final)] <- TRUE
+sce.nac$MSN.D1 <- NA
+sce.nac$MSN.D1[grep("MSN.D1", sce.nac$cellType.final)] <- TRUE
 
-sce.nac.noLNT$MSN.D1.0 <- NA
-sce.nac.noLNT$MSN.D1.0 <- sce.nac.noLNT$MSN.D1 & geneExprs["DRD1", ]==0
+sce.nac$MSN.D1.0 <- NA
+sce.nac$MSN.D1.0 <- sce.nac$MSN.D1 & geneExprs["DRD1", ]==0
 
 # Now plot & color by this - is DRD2 expressed?
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="MSN.D1.0", point_alpha=0.5, point_size=1.2, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
 
 
-sce.nac.noLNT$MSN.D2 <- NA
-sce.nac.noLNT$MSN.D2[grep("MSN.D2", sce.nac.noLNT$cellType.final)] <- TRUE
+sce.nac$MSN.D2 <- NA
+sce.nac$MSN.D2[grep("MSN.D2", sce.nac$cellType.final)] <- TRUE
 
-sce.nac.noLNT$MSN.D2.0 <- NA
-sce.nac.noLNT$MSN.D2.0 <- sce.nac.noLNT$MSN.D2 & geneExprs["DRD2", ]==0
+sce.nac$MSN.D2.0 <- NA
+sce.nac$MSN.D2.0 <- sce.nac$MSN.D2 & geneExprs["DRD2", ]==0
 
 # Now plot & color by this - is DRD1 expressed?
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="MSN.D2.0", point_alpha=0.5, point_size=1.2, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
@@ -577,26 +502,26 @@ plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DR
 
 
 # non-0 expression of BOTH? ===
-table(geneExprs["DRD1", union(which(sce.nac.noLNT$MSN.D1), which(sce.nac.noLNT$MSN.D2))] > 0 &
-        geneExprs["DRD2", union(which(sce.nac.noLNT$MSN.D1), which(sce.nac.noLNT$MSN.D2))] > 0)
+table(geneExprs["DRD1", union(which(sce.nac$MSN.D1), which(sce.nac$MSN.D2))] > 0 &
+        geneExprs["DRD2", union(which(sce.nac$MSN.D1), which(sce.nac$MSN.D2))] > 0)
 #FALSE  TRUE
 # 8017   819
 
-sce.nac.noLNT$MSN.broad <- NA
-sce.nac.noLNT$MSN.broad[grep("MSN", sce.nac.noLNT$cellType.final)] <- TRUE
+sce.nac$MSN.broad <- NA
+sce.nac$MSN.broad[grep("MSN", sce.nac$cellType.final)] <- TRUE
 # the above is the same as
-table(geneExprs["DRD1", which(sce.nac.noLNT$MSN.broad)] > 0 &
-        geneExprs["DRD2", which(sce.nac.noLNT$MSN.broad)] > 0)
+table(geneExprs["DRD1", which(sce.nac$MSN.broad)] > 0 &
+        geneExprs["DRD2", which(sce.nac$MSN.broad)] > 0)
 
 
 geneExprs.mat <- as.matrix(geneExprs)
-sce.nac.noLNT$bothD1D2 <- ifelse(geneExprs.mat["DRD1", sce.nac.noLNT$MSN.broad] > 0 &
-                                   geneExprs.mat["DRD2", sce.nac.noLNT$MSN.broad] > 0,
+sce.nac$bothD1D2 <- ifelse(geneExprs.mat["DRD1", sce.nac$MSN.broad] > 0 &
+                                   geneExprs.mat["DRD2", sce.nac$MSN.broad] > 0,
                                  TRUE, FALSE)
 
 
 # Now plot & color by this - are these high-expressing in some??
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="bothD1D2", point_alpha=0.5, point_size=1.2, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 25))
@@ -605,21 +530,21 @@ plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DR
 ## Plot these three subsets: ===
 pdf("pdfs/exploration/DRD1-vs-DRD2-exclusivity_all-NAc-n5_May202.pdf", height=6)
 # D1-MSN but 0 DRD1
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="MSN.D1.0", point_alpha=0.5, point_size=1.3, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 12)) +
   ggtitle(label="Classified with D1 subclusters but 0 DRD1 expression")
 
 # D2-MSN but 0 DRD2
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="MSN.D2.0", point_alpha=0.5, point_size=1.3, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 12)) +
   ggtitle(label="Classified with D2 subclusters but 0 DRD2 expression")
 
 # MSN but non-0 expression for BOTH
-plotExpression(sce.nac.noLNT, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
+plotExpression(sce.nac, exprs_values = "logcounts", features=c("DRD1", "DRD2"),
                x="cellType.final", colour_by="bothD1D2", point_alpha=0.5, point_size=1.3, ncol=5,
                add_legend=T) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(size = 12)) +
@@ -628,10 +553,10 @@ dev.off()
 
 
 # More exploration of these dual-expressing..??
-table(sce.nac.noLNT$prelimCluster, sce.nac.noLNT$bothD1D2)
+table(sce.nac$prelimCluster, sce.nac$bothD1D2)
 # don't seem to make up or be enriched in their own cluster
 
-table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$bothD1D2)
+table(sce.nac$cellType.final, sce.nac$bothD1D2)
 #          FALSE TRUE
 # Astro        0    0
 # Inhib.1      0    0
@@ -648,7 +573,7 @@ table(sce.nac.noLNT$cellType.final, sce.nac.noLNT$bothD1D2)
 # Oligo        0    0
 # OPC          0    0     
 
-table(sce.nac.noLNT$MSN.broad, sce.nac.noLNT$bothD1D2)
+table(sce.nac$MSN.broad, sce.nac$bothD1D2)
 #      FALSE TRUE
 # TRUE  8017  819
 
@@ -656,10 +581,10 @@ table(sce.nac.noLNT$MSN.broad, sce.nac.noLNT$bothD1D2)
 
 ## MNT 23Sep2020: Tabulating all regionally-defined clusters per donor ===
 # -> first make 'sce.n14', above
-sce.n14.noLNT <- sce.n14[ ,-grep("Ambig.lowNtrxts", sce.n14$cellType.RS.sub)]
-sce.n14.noLNT$cellType.RS.sub <- droplevels(sce.n14.noLNT$cellType.RS.sub)
+sce.n14 <- sce.n14[ ,-grep("Ambig.lowNtrxts", sce.n14$cellType.RS.sub)]
+sce.n14$cellType.RS.sub <- droplevels(sce.n14$cellType.RS.sub)
 
-RSsubs.donorTab <- as.data.frame.matrix(table(sce.n14.noLNT$cellType.RS.sub, sce.n14.noLNT$donor))
+RSsubs.donorTab <- as.data.frame.matrix(table(sce.n14$cellType.RS.sub, sce.n14$donor))
 # Trick some reordering by region
 RSsubs.donorTab$Region <- ss(rownames(RSsubs.donorTab),"_",2)
 RSsubs.donorTab$CellType <- paste0(RSsubs.donorTab$Region, "_", ss(rownames(RSsubs.donorTab),"_",1))
@@ -679,12 +604,12 @@ write.table(RSsubs.donorTab, file="tables/suppTable_68-RSsubclusters_donorStrati
             #  amy dlpfc   hpc   nac  sacc
             # 6632  5399 10444 13241  7047
         
-        table(sce.n14.noLNT$region)
+        table(sce.n14$region)
             #  amy dlpfc   hpc   nac  sacc
             # 6582  5231 10343 13148  7004
         
         # % Kept?
-        round(table(sce.n14.noLNT$region) / table(sce.n14$region),3)
+        round(table(sce.n14$region) / table(sce.n14$region),3)
             #   amy dlpfc   hpc   nac  sacc
             # 0.992 0.969 0.990 0.993 0.994
 
@@ -822,7 +747,7 @@ sce.rest$cellType.RS.sub <- droplevels(sce.rest$cellType.RS.sub)
 
 table(sce.rest$cellType.RS.sub)
 
-        RSsubs.donorTab <- as.data.frame.matrix(table(sce.n14.noLNT$cellType.RS.sub, sce.n14.noLNT$donor))
+        RSsubs.donorTab <- as.data.frame.matrix(table(sce.n14$cellType.RS.sub, sce.n14$donor))
         # Trick some reordering by region
         RSsubs.donorTab$Region <- ss(rownames(RSsubs.donorTab),"_",2)
         RSsubs.donorTab$CellType <- paste0(RSsubs.donorTab$Region, "_", ss(rownames(RSsubs.donorTab),"_",1))
