205c8c521812af080088da4f09927291ec475fe9
Matt N Tran <nnguye44@jhmi.edu>
2021-07-22 17:23:13 -0400

    sACC: compare to Velmeshev, et al. PFC/ACC profiles

--- 10x_sACC-n5_step04_comparison-publishedData_MNT.R
+++ 10x_sACC-n5_step04_comparison-publishedData_MNT.R
@@ -1,6 +1,6 @@
 ### MNT 10x snRNA-seq workflow: step 04
 ###   **Region-specific analyses**
-###     - (2x) sACC samples from: Br5161 & Br5212
+###     - (5x) sACC samples (M & F donors)
 ###     - Comparison to Velmeshev, et al (Science 2019)
 #####################################################################
 
@@ -35,40 +35,81 @@ load("/dcl01/ajaffe/data/lab/singleCell/velmeshev2019/analysis_MNT/markers-stats
 
 load("/dcl01/ajaffe/data/lab/singleCell/velmeshev2019/analysis_MNT/SCE_asd-velmeshev-etal_MNT.rda", verbose=T)
     # sce.asd, sce.asd.pfc, sce.asd.acc  
+sce.asd
+    # class: SingleCellExperiment 
+    # dim: 36501 104559 
+    # metadata(0):
+    # assays(1): logcounts
+    # rownames(36501): ENSG00000227232 ENSG00000243485 ... ENSG00000210195
+    # ENSG00000210196
+    # rowData names(0):
+    # colnames(104559): AAACCTGGTACGCACC-1_1823_BA24
+    #   AAACGGGCACCAGATT-1_1823_BA24 ... TTTGTCATCCCAAGTA-1_6033_BA9
+    #   TTTGTCATCGTTACGA-1_6033_BA9
+    # colData names(17): cell cluster ... BAregion contrast
+    # reducedDimNames(1): TSNE.prov
+    # altExpNames(0):
 
 sce.asd.pfc <- sce.asd[ ,sce.asd$region=="PFC"]
 sce.asd.acc <- sce.asd[ ,sce.asd$region=="ACC"]
-
+rm(sce.asd)
 
 # Need to convert Symbol in sce.dlpfc > EnsemblID, and also use n nuclei for t.stat
-load("rdas/regionSpecific_sACC-n2_cleaned-combined_SCE_MNTFeb2020.rda", verbose=T)
-    # sce.sacc, clusterRefTab.sacc, pc.choice.sacc, chosen.hvgs.sacc, ref.sampleInfo
+load("rdas/revision/regionSpecific_sACC-n5_cleaned-combined_SCE_MNT2021.rda", verbose=T)
+    # sce.sacc, chosen.hvgs.sacc, pc.choice.sacc, clusterRefTab.sacc, ref.sampleInfo, annotationTab.sacc, cell_colors.sacc
+sce.sacc
+    # class: SingleCellExperiment 
+    # dim: 33538 15669 
+    # metadata(2): merge.info pca.info
+    # assays(2): counts logcounts
+    # rownames(33538): MIR1302-2HG FAM138A ... AC213203.1 FAM231C
+    # rowData names(6): gene_id gene_version ... gene_biotype Symbol.uniq
+    # colnames(15669): AAACCCAAGAGTCTTC-1 AAACCCAAGGCCCGTT-1 ...
+    #   TTTGACTGTATCGTGT-1 TTTGGTTAGCAGCACA-1
+    # colData names(20): Sample Barcode ... collapsedCluster cellType
+    # reducedDimNames(4): PCA_corrected PCA_opt TSNE UMAP
+    # altExpNames(0):
 
-# Drop "Ambig.lowNtrxts" (43 nuclei)
-sce.sacc <- sce.sacc[ ,sce.sacc$cellType != "Ambig.lowNtrxts"]
+table(sce.sacc$cellType)
+    #   Astro_A        Astro_B   drop.doublet    drop.lowNTx        Excit_A        Excit_B 
+    #       747            160             28            298            856            575 
+    #   Excit_C        Excit_D        Excit_E        Excit_F        Excit_G        Inhib_A 
+    #      1735            311            428            228             30            842 
+    #   Inhib_B        Inhib_C        Inhib_D        Inhib_E        Inhib_F        Inhib_G 
+    #       912            465            384            330            521            206 
+    #   Inhib_H        Inhib_I        Inhib_J        Inhib_K          Micro Neu_FAT2.CDH15 
+    #       208             39             42             25            784             20 
+    #   Oligo_A        Oligo_B            OPC 
+    #      4389            195            911
+
+# Drop the flagged "drop." clusters 
+sce.sacc <- sce.sacc[ ,-grep("drop.", sce.sacc$cellType)]
 sce.sacc$cellType <- droplevels(sce.sacc$cellType)
 
-## Load LIBD sACC stats (just need the "1vAll" result)
-load("rdas/markers-stats_sACC-n2_findMarkers-SN-LEVEL_MNTMay2020.rda", verbose=T)
-    # markers.sacc.t.design, markers.sacc.wilcox.block, markers.sacc.binom.block, markers.sacc.t.1vAll
-    rm(markers.sacc.t.design, markers.sacc.wilcox.block, markers.sacc.binom.block)
 
-for(i in names(markers.sacc.t.1vAll)){
-  rownames(markers.sacc.t.1vAll[[i]]) <- rowData(sce.sacc)$ID[match(rownames(markers.sacc.t.1vAll[[i]]),
+## Load LIBD sACC stats (just need the "1vAll" result)
+load("rdas/revision/markers-stats_sACC-n5_findMarkers-SN-LEVEL_MNT2021.rda", verbose=T)
+    # markers.sacc.t.pw, markers.sacc.wilcox.block, markers.sacc.t.1vAll, medianNon0.sacc
+    rm(markers.sacc.t.pw, markers.sacc.wilcox.block, markers.sacc.binom.block)
+
+markers.sacc.enriched <- lapply(markers.sacc.t.1vAll, function(x){x[[2]]})
+    
+for(i in names(markers.sacc.enriched)){
+  rownames(markers.sacc.enriched[[i]]) <- rowData(sce.sacc)$gene_id[match(rownames(markers.sacc.enriched[[i]]),
                                                                     rownames(sce.sacc))]
 }
 
 
 ## Calculate and add t-statistic (= std.logFC * sqrt(N)) from contrasts
 #      and fix row order to the first entry "Astro"
-fixTo <- rownames(markers.sacc.t.1vAll[["Astro"]])
-for(s in names(markers.sacc.t.1vAll)){
-  markers.sacc.t.1vAll[[s]]$t.stat <- markers.sacc.t.1vAll[[s]]$std.logFC * sqrt(ncol(sce.sacc))
-  markers.sacc.t.1vAll[[s]] <- markers.sacc.t.1vAll[[s]][fixTo, ]
+fixTo <- rownames(markers.sacc.enriched[["Astro_A"]])
+for(s in names(markers.sacc.enriched)){
+  markers.sacc.enriched[[s]]$t.stat <- markers.sacc.enriched[[s]]$std.logFC * sqrt(ncol(sce.sacc))
+  markers.sacc.enriched[[s]] <- markers.sacc.enriched[[s]][fixTo, ]
 }
 
 # Pull out the t's
-ts.sacc <- sapply(markers.sacc.t.1vAll, function(x){x$t.stat})
+ts.sacc <- sapply(markers.sacc.enriched, function(x){x$t.stat})
 rownames(ts.sacc) <- fixTo
 
 
@@ -134,7 +175,7 @@ rownames(ts.velmeshev.pfc) <- fixTo
 
 sharedGenes.all <- intersect(rownames(ts.velmeshev.acc), rownames(ts.sacc))
 sharedGenes.all <- intersect(sharedGenes.all, rownames(ts.velmeshev.pfc))
-    # of length 27,422
+    # of length 27,890
 
 # Subset/order
 ts.sacc <- ts.sacc[sharedGenes.all, ]
@@ -155,10 +196,13 @@ range(cor_t_sacc.asd)
 regionInfo <- data.frame(region=ss(colnames(ts.velmeshev.full), "_",2))
 rownames(regionInfo) <- colnames(ts.velmeshev.full)
 
+# Re-name that "Neu_FAT2.CDH15" to "Neu_ambig", like for Fig 3
+rownames(cor_t_sacc.asd)[rownames(cor_t_sacc.asd)=="Neu_FAT2.CDH15"] <- "Neu_ambig"
+
 theSeq.all = seq(-.95, .95, by = 0.01)
 my.col.all <- colorRampPalette(brewer.pal(7, "BrBG"))(length(theSeq.all)-1)
 
-pdf("pdfs/exploration/Velmeshev-ASD_pfc-acc/overlap-velmeshev-ASD-bothRegions_with_LIBD-10x-sACC_Aug2020.pdf", width=10)
+pdf("pdfs/revision/exploration/overlap-velmeshev-ASD-bothRegions_with_LIBD-10x-sACC_MNT2021.pdf", width=10)
 pheatmap(cor_t_sacc.asd,
          color=my.col.all,
          annotation_col=regionInfo,
